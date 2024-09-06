00fbdd6dd1b4e48a9d13783a73135f0d2ef3a719
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-09-06 10:17:04 -0300

    Farmacia Navbar Home Footer Rotas

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar"
+import Home from "./pages/home/Home"
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[60vh]">
+          <Routes>
+            <Route path= '/' element={<Home />} />
+            <Route path= '/home' element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
