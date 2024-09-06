ca2064da512b6fe9579c1801ebb755894c6ea07c
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-09-06 11:39:51 -0300

    Consumo do Recurso Categoria

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Footer from "./components/footer/Footer"
 import Navbar from "./components/navbar/Navbar"
 import Home from "./pages/home/Home"
+import ListarCategorias from "./components/categorias/listarcategorias/ListarCategorias"
+import FormCategoria from "./components/categorias/formcategoria/FormCategoria"
+import DeletarCategoria from "./components/categorias/deletarcategorias/DeletarCategorias"
 
 function App() {
 
@@ -9,10 +12,14 @@ function App() {
     <>
       <BrowserRouter>
         <Navbar />
-        <div className="min-h-[60vh]">
+        <div className="min-h-[69vh] bg-gray-200">
           <Routes>
             <Route path= '/' element={<Home />} />
             <Route path= '/home' element={<Home />} />
+            <Route path="/categorias" element={<ListarCategorias />} />
+            <Route path="/cadcategoria" element={<FormCategoria />} />
+            <Route path="/editarcategoria/:id" element={<FormCategoria />} />
+            <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} /> 
           </Routes>
         </div>
         <Footer />
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -34,8 +34,8 @@ function Navbar() {
 
                     <div className='flex items-center gap-4 py-4'>
                         <Link to='#' className="hover:underline">Produtos</Link>
-                        <Link to='#' className="hover:underline">Categorias</Link>
-                        <Link to='#' className="hover:underline">Cadastrar Categoria</Link>
+                        <Link to='/categorias' className="hover:underline">Categorias</Link>
+                        <Link to='/cadcategoria' className="hover:underline">Cadastrar Categoria</Link>
                         <Link to='#' className="hover:underline">Sair</Link>
                         <Link to='#' ><User size={32} weight='bold' /></Link>
                         <Link to='#' ><ShoppingCart size={32} weight='bold' /></Link>
